dd7c501524cd6a9d6e5b05556458785521424ecf
Rene <altynai.mamytova17@gmail.com>
2022-12-26 14:44:25 +0600

    job parser

--- doska/spiders/job_spider.py
+++ doska/spiders/job_spider.py
@@ -1,5 +1,6 @@
 import scrapy
-import csv
+import json
+import datetime
 
 
 class JobUrlSpider(scrapy.Spider):
@@ -7,7 +8,7 @@ class JobUrlSpider(scrapy.Spider):
     base_url = "http://resume.doska.kg"
 
     def start_requests(self):
-        open('job_urls.csv', 'w').close()
+        open("job_urls.csv", "w").close()
 
         urls = ["http://resume.doska.kg/vacancy/&sortby=new",
                   "http://resume.doska.kg/vacancy/&page=2&sortby=new"]
@@ -16,10 +17,33 @@ class JobUrlSpider(scrapy.Spider):
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        filename = 'job_urls.csv'
-        with open(filename, 'a') as out:
+        filename = "job_urls.csv"
+        with open(filename, "a") as out:
             for job_title in response.css("div.list_full_title"):
                 job_url = job_title.css("a.title_url").attrib["href"]
-                full_url = self.base_url + job_url
+                full_url = self.base_url + ''.join(job_url.split('?')[:1])
                 out.write(full_url+"\n")
+
+
+class JobSpider(scrapy.Spider):
+    name = "jobs"
+    jobs = []
+
+    def start_requests(self):
+        with open("job_urls.csv", "r") as f:
+            for line in f:
+                yield scrapy.Request(url=line.rstrip(), callback=self.parse)
+
+    def parse(self, response):
+        filename = "jobs.json"
+        data = {
+            "category": response.css("div.kroshki").css("a[href*=cat]::text").get(),
+            "title": response.css("div.title::text").get(),
+            'description': ''.join(response.xpath("//div[contains(@class, 'desc')]/text()")[2:].getall()),
+            'city': response.xpath("//div[contains(@class, 'title')]/following-sibling::div[1]/text()").get(),
+            'contacts': {"phone number": response.xpath("//div[contains(@class, 'desc')]/text()")[1].get().strip()},
+            'created_on': datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
+        }
+        self.jobs.append(data)
+
 
