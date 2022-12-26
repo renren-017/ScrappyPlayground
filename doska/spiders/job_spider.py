38a7263cfd073544ab21a385e794419e390205a1
Rene <altynai.mamytova17@gmail.com>
2022-12-26 15:55:11 +0600

    Fixed: crawlers are running sequentially

--- doska/spiders/job_spider.py
+++ doska/spiders/job_spider.py
@@ -1,5 +1,4 @@
 import scrapy
-import json
 import datetime
 
 
--- main.py
+++ main.py
@@ -1,11 +1,19 @@
-import scrapy
 import json
-from scrapy.crawler import CrawlerProcess
+from twisted.internet import defer, reactor
+from scrapy.crawler import CrawlerRunner
 from doska.spiders import job_spider
 
-process = CrawlerProcess()
-process.crawl(job_spider.JobSpider)
-process.start()
+runner = CrawlerRunner()
 
+
+@defer.inlineCallbacks
+def crawl():
+    yield runner.crawl(job_spider.JobUrlSpider)
+    yield runner.crawl(job_spider.JobSpider)
+    reactor.stop()
+
+
+crawl()
+reactor.run()
 with open('jobs.json', 'w', encoding="utf-8") as json_file:
     json.dump(job_spider.jobs, json_file, indent=4, ensure_ascii=False)
